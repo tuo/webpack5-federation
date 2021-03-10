c23ab03c10ec594ca653f0c8859ff9c756d15c84
Tuo <tuo.huang@6e-group.com>
2021-03-10 14:24:52 +0800

    spike on custom sum.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import * as camelCase from "../node_modules/lodash/camelCase.js";
-
-console.log(camelCase("hello world"));
+// import * as camelCase from "../node_modules/lodash/camelCase.js";
+import { sum } from "./sum.js";
+// console.log(camelCase("hello world"));
+console.log("hell index.js sum: " + sum(2, 4));

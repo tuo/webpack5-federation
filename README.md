ae5a7cdbe7cec3b46d9111f01867b9ce1538a1d6
Tuo <tuo.huang@6e-group.com>
2021-03-10 14:38:03 +0800

    added scss error

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ http://localhost:8888/public/
 > > index.js:2 Uncaught SyntaxError: Cannot use import statement outside a module
 
 yarn add -D webpack webpack-cli
+
+add png
+https://juejin.cn/post/6844903648237977614
+
+add css/scss/less
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import "./style.scss";
+
 // import * as camelCase from "../node_modules/lodash/camelCase.js";
 import { sum } from "./sum.js";
+import { camelCase } from "lodash";
 // console.log(camelCase("hello world"));
 console.log("hell index.js sum: " + sum(2, 4));
 
@@ -7,3 +10,5 @@ let a = 5;
 let b = 10;
 
 console.log("5 + 10 " + sum(a, b));
+
+console.log(camelCase("hello world"));

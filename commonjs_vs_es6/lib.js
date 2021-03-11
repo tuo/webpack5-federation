e02b9e0c13aca5ec89f61760764589528c6f4d7f
Tuo <tuo.huang@6e-group.com>
2021-03-11 19:17:07 +0800

    added commonjs spike

---
+++ commonjs_vs_es6/lib.js
@@ -0,0 +1,28 @@
+var counter = 3;
+function incCounter() {
+    console.log("in - counter", counter);
+    console.log("in - id", module.id);
+    console.log("in - isloaded", module.loaded);
+    counter++;
+    console.log("in - counter omcr", counter);
+}
+// counter++;
+console.log("counter", counter);
+console.log("id", module.id);
+console.log("isloaded", module.loaded);
+
+setTimeout(() => {
+    console.log("2seconds  - ", counter);
+}, 2000);
+
+module.exports = {
+    counter,
+
+    incCounter: incCounter,
+};
+
+// get counter() {
+//   return counter;
+// },
+//https://es6.ruanyifeng.com/#docs/module-loader#ES6-%E6%A8%A1%E5%9D%97%E4%B8%8E-CommonJS-%E6%A8%A1%E5%9D%97%E7%9A%84%E5%B7%AE%E5%BC%82
+//https://www.cnblogs.com/unclekeith/p/7679503.html
---
+++ commonjs_vs_es6/main.js
@@ -0,0 +1,15 @@
+var counter = require("./lib").counter;
+var incCounter = require("./lib").incCounter;
+
+console.log("main ", counter); // 3
+// counter = 10;
+// console.log("main2 ", counter); // 10
+
+incCounter();
+console.log("main after ", counter); // 3
+console.log("main after import again ", require("./lib").counter); // 3
+
+setTimeout(() => {
+    console.log("main after 3 seconds", counter);
+    console.log("main after 3 seconds", require("./lib").counter);
+}, 3000);
